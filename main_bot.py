5f07637b77371738b3d26e2c483dd7b456d1de07
MoonDropX <minalinskyx@hotmail.com>
2017-07-31 13:08:44 +0900

    Add db connection

--- main_bot.py
+++ main_bot.py
@@ -1,7 +1,10 @@
 from dogbot.bot import bot
+from mongoengine import connect
+from config import config
 
 
 if __name__ == '__main__':
+    connect(config.get('db'))
     print("QQBot is running...")
     try:
         bot.start()
--- main_web.py
+++ main_web.py
@@ -1,5 +1,8 @@
 from dogbot.web import app
+from mongoengine import connect
+from config import config
 
 
 if __name__ == '__main__':
+    connect(config.get('db'))
     app.run(host='0.0.0.0', threaded=True)
