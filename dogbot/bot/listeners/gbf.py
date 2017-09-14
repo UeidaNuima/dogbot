c8c912d54b2baf6f8ff588b1c1b4417f79d73fcd
MoonDropX <minalinskyx@hotmail.com>
2017-09-14 23:38:07 +0900

    Add A-Z to regular expression; fix url without sechema

--- dogbot/bot/listeners/gbf.py
+++ dogbot/bot/listeners/gbf.py
@@ -1,5 +1,6 @@
 import re
 import requests
+from requests.exceptions import  MissingSchema
 from dogbot.cqsdk.utils import reply
 
 GBF_PATTERN = re.compile("(((https|http|ftp|rtsp|mms)?://)"  # 端口
@@ -7,10 +8,10 @@ GBF_PATTERN = re.compile("(((https|http|ftp|rtsp|mms)?://)"  # 端口
                          "(([0-9]{1,3}\.){3}[0-9]{1,3}"  # IP URL
                          "|"
                          "([0-9a-z_!~*'()-]+\.)*"  # 域名前綴
-                         "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\."  # 二級域名
+                         "([0-9a-zA-Z][A-Z0-9a-z-]{0,61})?[0-9a-zA-Z]\."  # 二級域名
                          "[a-z]{2,6})"  # 頂級域名
                          "(:[0-9]{1,4})?"  # 端口
-                         "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)*/?)")  # 路徑
+                         "(/[0-9a-zA-Z_!~*'().;?:@&=+$,%#-]+)*/?)")  # 路徑
 
 match_urls = []
 
@@ -24,7 +25,10 @@ def gbf(bot, message):
         if url.split('.')[-1] in ['png', 'jpg', 'jpeg', 'gif']:
             return False
         if url not in match_urls:
-            resp = requests.get(url)
+            try:
+                resp = requests.get(url)
+            except MissingSchema:
+                resp = requests.get('http://' + url)
             if 'granblue' in resp.url:
                 match_urls.append(url)
                 reply(bot, message, '碧蓝幻想，请勿访问')
