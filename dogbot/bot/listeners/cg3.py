a7ed65ba7991048ff794904dde6eb39d4b7604e4
MoonDropX <minalinskyx@hotmail.com>
2018-01-30 22:50:25 +0800

    Bugfix: display of all cg3

--- dogbot/bot/listeners/cg3.py
+++ dogbot/bot/listeners/cg3.py
@@ -23,16 +23,16 @@ def cg3(bot, message):
             reply(bot, message, '找不到单位{}...'.format(target_name))
             return True
         target = UnitUnpack.objects(Name=target.name).first()
-        if target.has_cg3:
+        if target.has_cg3():
             reply(bot, message, '{}, 有cg3'.format(target.Name))
         else:
             reply(bot, message, '{}, 没有cg3'.format(target.Name))
     else:
         targets = UnitUnpack.objects().all()
-        cg3s = [None, None, None, [], [], [], None, []]
+        cg3s = [[], [], [], [], [], [], [], []]
         for target in targets:
             if target.has_cg3():
                 cg3s[target.Rare].append(target.Name)
-        reply(bot, message, '金：\n{0[3]}\n白：\n{0[4]}\n黑：\n{0[5]}\n蓝：\n{0[7]}'.format(cg3s))
+        reply(bot, message, '金：\n{0[3]}\n白：\n{0[4]}\n黑：\n{0[5]}\n蓝：\n{0[7]}'.format(list(map(lambda names: ', '.join(names), cg3s))))
         return True
     return True
