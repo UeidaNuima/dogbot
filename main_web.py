4eae8b9f3350cd6a4716fbe85011732cbe6f3656
moondropx <minalinskyx@hotmail.com>
2018-02-13 23:38:43 +0900

    Bugfix

--- main_web.py
+++ main_web.py
@@ -2,7 +2,7 @@ from dogbot.web import app
 from mongoengine import connect
 from config import config
 
+connect(config.get('bot_db'), alias="bot")
 
 if __name__ == '__main__':
-    connect(config.get('bot_db'), alias="bot")
     app.run(host='0.0.0.0', threaded=True)
